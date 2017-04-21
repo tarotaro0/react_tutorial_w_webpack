0311f1ae3a927763ac87395d07fe3b6fe154ac00
tarotaro0 <tarousann11922960@yahoo.co.jp>
2017-04-21 11:46:52 +0900

    remove hello name

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import HelloMessage, {GoodbyeMessage} from './new.js'
 import {CommentBox} from './react_tutorial.js'
-
-ReactDOM.render(
-  (<HelloMessage name={"100"}/>),
-  document.getElementById('container')
-)
 
 ReactDOM.render(
   <CommentBox url="/comments" pollInterval={2000} />,
